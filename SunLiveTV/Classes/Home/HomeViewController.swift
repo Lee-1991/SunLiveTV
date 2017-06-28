0a47d6d24ffaf4c0432bdf4c53345d563909e9d8
lee <lee@wanba123.cn>
2017-06-28 11:50:18 +0800

    粒子动画

--- SunLiveTV/Classes/Home/HomeViewController.swift
+++ SunLiveTV/Classes/Home/HomeViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+//1.创建发射器
+let emitter = CAEmitterLayer()
+
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -21,7 +24,14 @@ class HomeViewController: UIViewController {
         tipsLab.textAlignment = .center
         self.view.addSubview(tipsLab)
         
+        //动画按钮
+        let animatedBtn = UIButton(frame: CGRect(x: 200, y: 100, width: 100, height: 50))
+        animatedBtn.setTitle("animation", for: .normal)
+        animatedBtn.backgroundColor = UIColor.red
+        animatedBtn.addTarget(self, action:#selector(HomeViewController.clickedAnimatesBtn), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(animatedBtn)
         
+        animation()
         // Do any additional setup after loading the view.
     }
 
@@ -30,15 +40,75 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func clickedAnimatesBtn() {
+        if emitter.birthRate == 0 {
+            emitter.birthRate = 1
+        }
+        else
+        {
+            emitter.birthRate = 0
+        }
     }
-    */
+    
+
+    func animation() {
+        
+        
+        //2.设置发射器的位置
+        emitter.emitterPosition=CGPoint(x:self.view.bounds.width*0.5, y:self.view.bounds.height/2)
+        
+//        emitter.emitterSize = CGSize(width:100,height:100)
+        //3.开启三维效果
+        emitter.preservesDepth=true
+        
+        let cellHome = getEmitterCell("home_home_select")
+        let cellStar = getEmitterCell("home_follow_select")
+        let cellFind = getEmitterCell("home_find_select")
+        let cellMine = getEmitterCell("home_mine_select")
+        
+        //将粒子加入发射器中
+//        let rect = CGRect(x: self.view.bounds.width/2, y: self.view.bounds.height/2, width: 100, height: 100)
+//        
+//        let disPlayView = UIView(frame:rect)
+//        disPlayView.layer.frame = rect;
+//        //将发射器的层加入父类layer的层上
+        self.view.layer.addSublayer(emitter)
+//        self.view.addSubview(disPlayView)
+        emitter.emitterCells = [cellHome,cellStar,cellFind,cellMine]
+        
+    }
+    
+    
+    func getEmitterCell(_ imageName: NSString) -> CAEmitterCell {
+        
+        //4.创建粒子，设置相关属性
+        let cell = CAEmitterCell()
+        
+        //速度
+        cell.velocity=150
+        
+        //速度范围的波动
+        cell.velocityRange=100
+        
+        //设置粒子的大小
+        cell.scale=1.0
+        cell.scaleRange=0.3
+        
+        //设置粒子的方向
+        cell.emissionLongitude = -(.pi)
+        cell.emissionRange = .pi
+        
+        //设置粒子的存活时间
+        cell.lifetime = 0.5
+        cell.lifetimeRange = 0.5
+        
+        //设置粒子每秒弹出的个数
+        cell.birthRate=1
+        
+        //设置粒子展示的图片
+        cell.contents=UIImage(named:imageName as String)?.cgImage
+        
+        return cell
+    }
 
 }
